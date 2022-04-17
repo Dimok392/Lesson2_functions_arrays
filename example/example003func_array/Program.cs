f83887c8406ec107a858228ccc8cc599c4cbd45d
Пётр <dimok392@mail.ru>
2022-04-18 00:05:40 +0300

    example003func-array

---
+++ example/example003func_array/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1,2,1,4,3,6,5,4,8,6,4};
+int n = array.Length; // определяет длину массива
+int find = 4;
+int index = 0;
+
+while (index<n)
+{
+    if(array[index] == find)
+{
+    System.Console.WriteLine(index);
+    break; //в случае выполнения if(array[index] == find) заканчивает проверку.
+}
+index++;
+
+}
+
+
